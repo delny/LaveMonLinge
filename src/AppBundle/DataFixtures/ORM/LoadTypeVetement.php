f065b7e7689a28762e5c3a4ce550e1f2168d4ef0
dylan <aguddylan@gmail.com>
2017-04-12 12:22:51 +0200

    Ajout des fixtures typeclothing,user,orderlaundry

---
+++ src/AppBundle/DataFixtures/ORM/LoadTypeVetement.php
@@ -0,0 +1,51 @@
+<?php
+namespace AppBundle\DataFixtures\ORM;
+use AppBundle\Entity\TypeClothing;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use AppBundle\Entity\TypeVetement;
+class LoadTypeVetementData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $datas = [
+
+            [
+                'label'=>'Chemise',
+                'prix'=>5,
+
+            ],
+
+
+            [
+                'label'=>'Pantalon',
+                'prix'=>6,
+
+
+            ],
+
+            [
+                'label'=>'Jupe',
+                'prix'=>12,
+
+
+            ],
+        ];
+        foreach ($datas as $i => $data) {
+            $typeclothing = new TypeClothing();
+            $typeclothing->setLabel($data['label']);
+            $typeclothing->setPrice($data['prix']);
+
+
+            $manager->persist($typeclothing);
+            $this->addReference('-'.$i, $typeclothing);
+        }
+        $manager->flush();
+    }
+    public function getOrder()
+    {
+        return 3;
+    }
+}
+
