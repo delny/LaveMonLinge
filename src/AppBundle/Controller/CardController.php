9a8e118a10ca22de9be8c45992b54db5c5612d28
dylan <aguddylan@gmail.com>
2017-04-19 14:35:14 +0200

    Solution de paiement par carte bancaire

--- src/AppBundle/Controller/CardController.php
+++ src/AppBundle/Controller/CardController.php
@@ -25,6 +25,90 @@ class CardController extends Controller
         return $this->render(':lavage:list.html.twig', array('form' => $form->createView()));
     }
 
+
+    /**
+     * @Route("/payment", name="app_payment")
+     */
+
+    public function prepareAction()
+    {
+        //A remplacer plus tard par le total récupéré une fois la commande réalisée
+        $total = 10;
+        $total .= "00";
+        //Pour mettre le montant exact dans Sprite qui sera en centimes
+
+        return $this->render(':paiement:paiement.html.twig', [
+            'total' => $total,
+        ]);
+
+    }
+
+    /**
+     * @Route(
+     *     "/checkout",
+     *     name="app_payment_send",
+     *     methods="POST"
+     * )
+     */
+
+    public function checkoutAction()
+    {
+        \Stripe\Stripe::setApiKey("sk_test_exem4WQHP8Jjcb7LfKyMgDlJ ");
+
+        // Get the credit card details submitted by the form
+
+        $email = $_POST['stripeEmail'];
+        $token = $_POST['stripeToken'];
+
+
+        $user = $this->getUser();
+        //Si c'est un nouveau client
+        if ($user->getToken() === NULL || empty($user->getToken())) {
+            // Create a Customer:
+            $customer = \Stripe\Customer::create(array(
+                "email" => $email,
+                "source" => $token,
+            ));
+            // Create a charge
+            try {
+                $charge = \Stripe\Charge::create(array(
+                    "amount" => 1000, // Centimes
+                    "currency" => "eur",
+                    "description" => "Paiement de" . $email,
+                    "customer" => $customer->id,
+
+
+                ));
+                $user->setToken($customer->id);
+                $this->get('app.user_manager')->save($user);
+                $this->addFlash("success", "Paiement accepté !");
+                return $this->redirectToRoute("app_payment");
+            } catch (\Stripe\Error\Card $e) {
+
+                $this->addFlash("error", "Paiement refusé .");
+                return $this->redirectToRoute("app_payment");
+                // The card has been declined
+            }
+        } else {
+            try {
+                $charge = \Stripe\Charge::create(array(
+                    "amount" => 1000,
+                    "currency" => "eur",
+                    "customer" => $user->getToken(),
+                ));
+                $this->addFlash("success", "Paiement accepté bis !");
+                return $this->redirectToRoute("app_payment");
+            } catch (\Stripe\Error\Card $e) {
+                $this->addFlash("error", "Paiement refusé .");
+                return $this->redirectToRoute("app_payment");
+                // The card has been declined
+            }
+        }
+    }
+
+
+
+
     private function getManager($manager){
         return $this->get($manager);
     }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -55,6 +55,16 @@ class User implements UserInterface
      */
     private $address;
 
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=255)
+     *
+     */
+
+    private $token;
+
     /**
      * @return int
      */
@@ -118,6 +128,8 @@ class User implements UserInterface
     {
         $this->address = $address;
     }
+
+
 
     /**
      * @return bool
@@ -183,4 +195,23 @@ class User implements UserInterface
     {
         $this->address->removeElement($address);
     }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+
+
 }
