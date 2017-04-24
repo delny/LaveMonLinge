9e573bcc04f9cfd0f488d83fae7ab1775feb8832
anthony <delgado.anthony@gmail.com>
2017-04-24 15:58:09 +0200

    creation formulaire en cours

--- src/AppBundle/Controller/BasketController.php
+++ src/AppBundle/Controller/BasketController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Manager\BasketManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BasketController extends Controller
@@ -12,14 +13,21 @@ class BasketController extends Controller
     /**
      * @Route("/basket", name="app_showBasket")
      */
-    public function showBasketAction(){
+    public function showBasketAction(Request $request){
         $basket = $this->getManager('app.basket_manager')->getBasket();
 
+        //appel manager
+
+
+        //instance
+
+        //creation formulaire
 
 
 
         return $this->render(':panier:panier.html.twig', ['basket' => $basket]);
     }
+
 
     private function getManager($manager){
         return $this->get($manager);
